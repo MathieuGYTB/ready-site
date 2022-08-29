29db32a1d3bd7ec6b3d660941d74a83924c3d318
MathieuGYTB <mat.68@orange.fr>
2022-08-29 10:57:41 +0200

    add table in user.html.twig

--- symfony/src/Controller/UserController.php
+++ symfony/src/Controller/UserController.php
@@ -2,9 +2,16 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Bill;
+use App\Repository\BillRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\Persistence\ManagerRegistry;
 
 class UserController extends AbstractController
 {
@@ -17,7 +24,28 @@ class UserController extends AbstractController
       $nom = ucfirst($nom);
       $prenom = $this->getUser()->getPrenom();
       $prenom = ucfirst($prenom);
+      $email = $this->getUser()->getEmail();
+      //$id = $bill->findBy($id);
+      //$description = $bill->findBy($description);
+      //$quantity = $bill->findBy($quantity);
+      //$price = $bill->findBy($price);
+      //$money = $bill->findBy($money);
 
-      return $this->render(view: 'user/user.html.twig', parameters:['nom'=> $nom, 'prenom' => $prenom]);
+      //if (!$id) {
+      //  throw $this->createNotFoundException(
+      //      'Pas de facture '
+      //  );
+      //}
+
+      return $this->render(view: 'user/user.html.twig', parameters:[
+        'nom'=> $nom,
+        'prenom' => $prenom,
+        'email' => $email
+        //'id' => $id,
+        //'description' => $description,
+        //'quantité' => $quantity,
+        //'prix' => $price,
+        //'monaie' => $money
+      ]);
     }
 }
