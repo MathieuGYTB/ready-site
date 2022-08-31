c4a09354c8221905945a5cb1af21ff3a743a9a1f
MathieuGYTB <mat.68@orange.fr>
2022-08-31 14:16:09 +0200

    delete js files

--- symfony/public/js/script.js
+++
@@ -1,9 +0,0 @@
-const checkbox = document.getElementsByTagName('_show_password');
-let inputPassword = document.getElementById('inputPassword');
-
-//function to show and hide password
-checkbox.addEventListener('click', () => {
-    inputPassword.type === inputPassword.type === "password" ? "text" : "password";
-  }
-)
-
