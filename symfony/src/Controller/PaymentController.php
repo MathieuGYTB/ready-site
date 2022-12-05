02935b06e7b644ea23b19f9195e69e815937133a
MathieuGYTB <mat.68@orange.fr>
2022-12-05 18:31:22 +0100

    add PaymentController

---
+++ symfony/src/Controller/PaymentController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PaymentController extends AbstractController
+{
+    #[Route('/checkout', name: 'app_checkout')]
+    public function checkout($stripeSK): Response
+    {
+        \Stripe\Stripe::setApiKey($stripeSK);
+
+        $YOUR_DOMAIN = 'http://symfony.localhost';
+
+        $checkout_session = \Stripe\Checkout\Session::create([
+            'line_items' => [[
+                # Provide the exact Price ID (e.g. pr_1234) of the product you want to sell
+                'price' => 'price_1MBZBXKgHxrl7uH3ALEzb4uD',
+                'quantity' => 1,
+            ]],
+            'mode' => 'payment',
+            'success_url' => $YOUR_DOMAIN . '/success.html',
+            'cancel_url' => $YOUR_DOMAIN . '/cancel.html',
+            'automatic_tax' => [
+                'enabled' => true,
+            ],
+        ]);
+
+        return $this->redirect($checkout_session->url, 303);
+    }
+}
