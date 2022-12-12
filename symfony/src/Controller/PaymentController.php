d6ac630424380d267002101bf41e8b1f47909d69
MathieuGYTB <mat.68@orange.fr>
2022-12-12 15:39:07 +0100

    add setRoles in paymentController again

--- symfony/src/Controller/PaymentController.php
+++ symfony/src/Controller/PaymentController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
+use Stripe\StripeObject;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,20 +17,18 @@ class PaymentController extends AbstractController
     {
         if ($_ENV['APP_ENV'] == 'dev') {
             $stripeSK = $_ENV['STRIPE_TEST_SECRET_KEY'];
-        } else 
-        {
+        } else {
             $stripeSK = $_ENV['STRIPE_PROD_SECRET_KEY'];
         }
         \Stripe\Stripe::setApiKey($stripeSK);
 
         $YOUR_DOMAIN = 'http://symfony.localhost';
         $user_email = $this->getUser()->getEmail();
-
         $product_price = $_ENV['PRODUCT_PRICE'];
         $admin_siret = $_ENV['ADMIN_SIRET'];
         $code_ape = $_ENV['CODE_APE'];
         $tva = $_ENV['TVA'];
-        
+
         $checkout_session = \Stripe\Checkout\Session::create([
             'billing_address_collection' => "required",
             'custom_text' => [
@@ -40,11 +40,13 @@ class PaymentController extends AbstractController
                 'terms_of_service' => 'required',
             ],
             'customer_email' => $user_email,
-            'line_items' => [[
-                # Provide the exact Price ID (e.g. pr_1234) of the product you want to sell
-                'price' => $product_price,
-                'quantity' => 1,
-            ]],
+            'line_items' => [
+                [
+                    # Provide the exact Price ID (e.g. pr_1234) of the product you want to sell
+                    'price' => $product_price,
+                    'quantity' => 1,
+                ]
+            ],
             'mode' => 'payment',
             'allow_promotion_codes' => true,
             'invoice_creation' => [
@@ -66,7 +68,7 @@ class PaymentController extends AbstractController
                     ],
                 ],
             ],
-            'success_url' => $YOUR_DOMAIN . "/profile/kgfnhtl1616gbvh",
+            'success_url' => $YOUR_DOMAIN . "/profile/kgfnhtl1616gbvh?session_id={CHECKOUT_SESSION_ID}",
             'cancel_url' => $YOUR_DOMAIN . "/",
             'automatic_tax' => [
                 'enabled' => true,
@@ -74,12 +76,17 @@ class PaymentController extends AbstractController
         ]);
 
         return $this->redirect($checkout_session->url, 303);
-        
+
     }
 
     #[Route("{{ path(''app_succes')}}", name: 'app_success')]
-    public function success(): Response
+    public function success(EntityManagerInterface $entityManagerInterface): Response
     {
+        $user = $this->getUser();
+        $user->setRoles(['ROLE_PAYED']);
+        $entityManagerInterface->persist($user);
+        $entityManagerInterface->flush();
+
         return $this->render(view: 'payment/success.html.twig');
     }
 }
